ceac22fef603a67790a70a699a664505026d7957
kimsj00 <tmdwo8814@gmail.com>
2024-01-12 16:53:28 +0900

    FEAT 김승재.py

--- 1주차/김승재.py
+++ 1주차/김승재.py
@@ -16,8 +16,12 @@ def solution(name):
             else:
                 if index < 0:
                     left += 1
+                    index -= 1
+                    continue
                 else:
                     right += 1
+                    index += 1
+                    continue
 
         # name[index]가 'A'가 아닌 경우
         else:
@@ -27,11 +31,13 @@ def solution(name):
                 down += 26 - (ord(name[index]) - ord('A'))
             else:
                 up += 13
-
-        if index > 0 or (index == 0 and up > 0):
+                
+        if name[index : len(name) - count] == 'A' * (len(name) - count):
+            return up + down + left + right
+        elif index > 0 or (index == 0 and name[-1] == 'A'):
             index += 1
             right += 1
-        elif index < 0 or (index == 0 and down > 0):
+        elif index < 0 or (index == 0 and name[1] == 'A'):
             index -= 1
             left += 1
 
@@ -39,3 +45,5 @@ def solution(name):
             
             
     return up + down + left + right
+
+print(solution('JAZ'))
